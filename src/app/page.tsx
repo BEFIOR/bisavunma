bb73874f4b4f9eef518f42391941cc0214c0fe02
mach2furkan <furkanasknn@gmail.com>
2025-08-26 09:55:59 +0300

    deneme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-center">
-        Bi Savunma
+        BiSavunma Proje
       </h1>
     </div>
   );
